996a807e2d179419507e87ad9801ead04a3d2169
yangankang <yak1992@foxmail.com>
2019-09-22 16:53:17 +0800

    提交

--- mimosa-orm/src/main/java/org/mimosaframework/orm/mapping/NothingCompareMapping.java
+++ mimosa-orm/src/main/java/org/mimosaframework/orm/mapping/NothingCompareMapping.java
@@ -11,6 +11,11 @@ public class NothingCompareMapping implements StartCompareMapping {
     public NothingCompareMapping(ActionDataSourceWrapper dataSourceWrapper, NotMatchObject notMatchObject) {
         this.dataSourceWrapper = dataSourceWrapper;
         this.notMatchObject = notMatchObject;
+
+        if (notMatchObject.getMimosaDataSource() != null) {
+            dataSourceWrapper.setDataSource(notMatchObject.getMimosaDataSource());
+            dataSourceWrapper.setMaster(true);
+        }
     }
 
     @Override
