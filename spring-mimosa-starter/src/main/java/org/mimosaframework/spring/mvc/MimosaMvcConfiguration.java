42d1bbad8be6ebe8d27532605d4dc68569efd30d
yangankang <yak1992@foxmail.com>
2020-07-28 13:10:32 +0800

    提交

--- spring-mimosa-starter/src/main/java/org/mimosaframework/spring/mvc/MimosaMvcConfiguration.java
+++ spring-mimosa-starter/src/main/java/org/mimosaframework/spring/mvc/MimosaMvcConfiguration.java
@@ -1,10 +1,12 @@
 package org.mimosaframework.spring.mvc;
 
 import org.mimosaframework.orm.MimosaSessionTemplate;
+import org.mimosaframework.orm.spring.SpringMimosaSessionTemplate;
 import org.mimosaframework.springmvc.MimosaHandlerMethodExceptionResolver;
 import org.mimosaframework.springmvc.MimosaRequestHandlerAdapter;
 import org.mimosaframework.springmvc.MimosaRequestHandlerMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,16 +15,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExc
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import javax.annotation.Resource;
+
 @Configuration
 @ConditionalOnClass({RequestMappingHandlerMapping.class, RequestMappingHandlerAdapter.class})
+@ConditionalOnBean({MimosaSessionTemplate.class})
 @EnableConfigurationProperties({MimosaMvcProperties.class})
 public class MimosaMvcConfiguration implements WebMvcRegistrations {
 
     @Autowired
     MimosaMvcProperties mimosaMvcProperties;
 
-    @Autowired
-    private MimosaSessionTemplate mimosaSessionTemplate;
+    private SpringMimosaSessionTemplate mimosaSessionTemplate;
+
+    @Resource
+    public void setMimosaSessionTemplate(SpringMimosaSessionTemplate mimosaSessionTemplate) {
+        this.mimosaSessionTemplate = mimosaSessionTemplate;
+    }
 
     @Override
     public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
