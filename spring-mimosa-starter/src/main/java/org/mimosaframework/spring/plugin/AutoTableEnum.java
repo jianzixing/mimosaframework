b3cbfc56a723d91a7413dde56183c3d33944dd1c
qmt216 <qmt216@foxmail.com>
2023-04-26 10:51:29 +0800

    想弄个maven插件生成表对应的枚举类

---
+++ spring-mimosa-starter/src/main/java/org/mimosaframework/spring/plugin/AutoTableEnum.java
@@ -0,0 +1,17 @@
+package org.mimosaframework.spring.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * desc 自动创建表结构的枚举类
+ * @goal generate_table
+ *
+ */
+public class AutoTableEnum extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("generate_table start run");
+    }
+}
