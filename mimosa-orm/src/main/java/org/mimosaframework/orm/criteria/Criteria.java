490f2d75009e6b9b5b362cca1c3b2b6d9f1d8459
yangankang <yak1992@foxmail.com>
2023-10-25 18:05:53 +0800

    提交

--- mimosa-orm/src/main/java/org/mimosaframework/orm/criteria/Criteria.java
+++ mimosa-orm/src/main/java/org/mimosaframework/orm/criteria/Criteria.java
@@ -7,61 +7,69 @@ import java.io.Serializable;
  */
 public final class Criteria {
 
-    public static final Query<LogicQuery> query(Class c) {
+    public static Query<LogicQuery> query(Class c) {
         return new DefaultQuery(c);
     }
 
-    public static final LogicQuery logicQuery(Class c) {
+    public static LogicQuery logicQuery(Class c) {
         return new DefaultQuery(c);
     }
 
-    public static final DefaultFilter filter() {
-        return new DefaultFilter();
-    }
-
-    public static final Join join(Class c) {
-        return new DefaultJoin(c, 0);
-    }
-
-    public static final Join left(Class c) {
-        return new DefaultJoin(c, 0);
-    }
-
-    public static final Join inner(Class c) {
-        return new DefaultJoin(c, 1);
-    }
-
-    public static final Update<LogicUpdate> update(Class c) {
-        return new DefaultUpdate(c);
-    }
-
-    public static final Delete<LogicDelete> delete(Class c) {
+    public static LogicDelete logicDelete(Class c) {
         return new DefaultDelete(c);
     }
 
-    public static final Function<LogicFunction> fun(Class c) {
+    public static LogicUpdate logicDUpdate(Class c) {
+        return new DefaultUpdate(c);
+    }
+
+    public static DefaultFilter filter() {
+        return new DefaultFilter();
+    }
+
+    public static Join join(Class c) {
+        return new DefaultJoin(c, 0);
+    }
+
+    public static Join left(Class c) {
+        return new DefaultJoin(c, 0);
+    }
+
+    public static Join inner(Class c) {
+        return new DefaultJoin(c, 1);
+    }
+
+    public static Update<LogicUpdate> update(Class c) {
+        return new DefaultUpdate(c);
+    }
+
+    public static Delete<LogicDelete> delete(Class c) {
+        return new DefaultDelete(c);
+    }
+
+    public static Function<LogicFunction> fun(Class c) {
         return new DefaultFunction(c);
     }
 
-    public static final WrapsLinked<LogicWrapsLinked> linked() {
+    public static WrapsLinked<LogicWrapsLinked> linked() {
         return new DefaultWrapsLinked();
     }
 
-    public static final LogicWrapsLinked logicLinked() {
+    public static LogicWrapsLinked logicLinked() {
         return new DefaultWrapsLinked();
     }
 
-    public static final LogicWrapsLinked linked(WrapsLinked<LogicWrapsLinked> l) {
+    public static LogicWrapsLinked linked(WrapsLinked<LogicWrapsLinked> l) {
         DefaultWrapsLinked linked = new DefaultWrapsLinked();
         linked.linked(l);
         return linked;
     }
 
-    public static final <T> UpdateObject wrapUpdate(T t) {
+    public static <T> UpdateObject wrapUpdate(T t) {
         return new UpdateObject(t);
     }
 
-    public static final AsField as(String alias, Serializable field) {
+    public static AsField as(String alias, Serializable field) {
         return new AsField(alias, field);
     }
 
