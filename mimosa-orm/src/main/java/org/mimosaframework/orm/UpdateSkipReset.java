3c984f5662e8bc1a95ef7bc958329947e4eafb2e
yangankang <yak1992@foxmail.com>
2025-04-20 15:10:45 +0800

    提交

--- mimosa-orm/src/main/java/org/mimosaframework/orm/UpdateSkipReset.java
+++ mimosa-orm/src/main/java/org/mimosaframework/orm/UpdateSkipReset.java
@@ -3,6 +3,8 @@ package org.mimosaframework.orm;
 import org.mimosaframework.core.json.ModelObject;
 import org.mimosaframework.orm.mapping.MappingTable;
 
+import java.util.List;
+
 public interface UpdateSkipReset {
-    void skip(ModelObject object, MappingTable mappingTable);
+    void skip(ModelObject object, MappingTable mappingTable, List<String> usedFields);
 }
