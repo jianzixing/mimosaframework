4a72b8a33000e02c8a10b85d152ccf8b6765a80c
杨安康 <yak1992@foxmail.com>
2025-10-31 14:26:04 +0800

    提交

--- mimosa-core/src/main/java/org/mimosaframework/core/exception/ModuleException.java
+++ mimosa-core/src/main/java/org/mimosaframework/core/exception/ModuleException.java
@@ -1,11 +1,14 @@
 package org.mimosaframework.core.exception;
 
+import java.util.Map;
+
 /**
  * @author yangankang
  */
 public class ModuleException extends RuntimeException {
 
     private Object code;
+    private Map<String, String> args;
 
     public ModuleException(ModelCheckerException e) {
         super(e.getMessage());
@@ -65,6 +68,13 @@ public class ModuleException extends RuntimeException {
         this.code = code;
     }
 
+
+    public ModuleException(Object code, Map<String, String> args) {
+        super();
+        this.code = code;
+        this.args = args;
+    }
+
     public ModuleException(Object code, String message, Throwable throwable) {
         super(message, throwable);
         this.code = code;
@@ -72,5 +82,9 @@ public class ModuleException extends RuntimeException {
 
     public Object getCode() {
         return code;
+    }
+
+    public Map<String, String> getArgs() {
+        return args;
     }
 }
