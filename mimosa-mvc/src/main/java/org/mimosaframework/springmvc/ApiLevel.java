97ec338ae800882a390ec37b906ec134a93c697a
杨安康 <yak1992@foxmail.com>
2025-11-07 16:02:34 +0800

    提交

--- mimosa-mvc/src/main/java/org/mimosaframework/springmvc/ApiLevel.java
+++ mimosa-mvc/src/main/java/org/mimosaframework/springmvc/ApiLevel.java
@@ -18,25 +18,29 @@ public enum ApiLevel {
      */
     PROJECT(3),
     /**
+     * 官网级权限鉴权
+     */
+    OFFICIAL(4),
+    /**
      * 严格模式，无含义自行使用
      */
-    STRICT(4),
+    STRICT(5),
     /**
      * 使用密钥访问鉴权
      */
-    KEY(5),
+    KEY(6),
     /**
      * 使用Token访问鉴权
      */
-    TOKEN(6),
+    TOKEN(7),
     /**
      * 内部API调用鉴权
      */
-    CALL(7),
+    CALL(8),
     /**
      * 多步验证
      */
-    NFA(8),
+    NFA(9),
     ;
 
     private final int value;
